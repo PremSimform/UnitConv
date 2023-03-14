a8c217c1983fa8701570026e691c7f37ca4279e3
PremSimform <122260605+PremSimform@users.noreply.github.com>
2023-03-14 19:10:44 +0530

    Upload Version 1

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title></title>
+  <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"><link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css'>
+<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css'><link rel="stylesheet" href="./style.css">
+
+
+</head>
+<body>
+<!-- partial:index.partial.html -->
+<nav class="navbar navbar-expand-custom navbar-mainbg">
+            <a class="navbar-brand navbar-logo" href="#">Measurement Converter</a>
+            <button class="navbar-toggler" type="button" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <i class="fas fa-bars text-white"></i>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ml-auto">
+                    <div class="hori-selector">
+                        <div class="left"></div>
+                        <div class="right"></div>
+                    </div>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="about.php"><i class="fa fa-address-card"></i>About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="ld.php"><i class="fa fa-ruler"></i>Length & Distance</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="area.php"><i class="fa fa-clone"></i>Area</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="vol.php"><i class="fa fa-prescription-bottle"></i>Volume and Capacity</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="mass.php"><i class="fa-solid fa-scale-unbalanced-flip"></i>Mass and Weight</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="speed.php"><i class="fa fa-tachometer-average"></i>Speed</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="temp.php"><i class="fa fa-temperature-high"></i>Temprature</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+<!-- partial -->
+  <script src='https://code.jquery.com/jquery-3.4.1.min.js'></script>
+<script src='https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js'></script>
+<script src='https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.min.js'></script><script  src="./script.js"></script>
+
+</body>
+</html>
---
+++ ld.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Length & Distance</title>
+</head>
+
+<body>
+    <!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css'>
+        <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css'>
+        <link rel="stylesheet" href="./style.css">
+        <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" > -->
+
+
+    </head>
+
+    <body>
+        <!-- partial:index.partial.html -->
+        <nav class="navbar navbar-expand-custom navbar-mainbg">
+            <a class="navbar-brand navbar-logo" href="index.php">Measurement Converter</a>
+            <button class="navbar-toggler" type="button" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <i class="fas fa-bars text-white"></i>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ml-auto">
+                    <div class="hori-selector">
+                        <div class="left"></div>
+                        <div class="right"></div>
+                    </div>
+                    <li class="nav-item">
+                        <a class="nav-link" href="about.php"><i class="fa fa-address-card"></i>About</a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="ld.php"><i class="fa fa-ruler"></i>Length & Distance</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="area.php"><i class="fa fa-clone"></i>Area</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="vol.php"><i class="fa fa-prescription-bottle"></i>Volume and Capacity</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="mass.php"><i class="fa-solid fa-scale-unbalanced-flip"></i>Mass and Weight</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="speed.php"><i class="fa fa-tachometer-average"></i>Speed</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="temp.php"><i class="fa fa-temperature-high"></i>Temprature</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        <div id="form">
+            <form style="width: 80%;">
+                <div class="form-row">
+                    <div class="form-group col-md-6">
+                        <label for="input1">Length-1</label>
+                        <input type="number" class="form-control" id="input1" placeholder="Enter Number">
+                    </div>
+                    <div class="form-group col-md-6">
+                        <label for="inputunit1">Unit</label>
+                        <!-- <input type="password" class="form-control" id="inputPassword4" placeholder="Password"> -->
+                        <select class="form-control" id="inputunit1">
+                            <option id="km">Kilometer</option>
+                            <option id="ml">Miles</option>
+                            <option id="mtr">Meters</option>
+                            <option id="cm">Centimeter</option>
+                            <option id="mm">Milimeter</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="form-row">
+                    <div class="form-group col-md-6">
+                        <label for="input2">Length-2</label>
+                        <input type="number" class="form-control" id="input2" placeholder="Enter Number">
+                    </div>
+                    <div class="form-group col-md-6">
+                        <label for="inputunit2">Unit</label>
+                        <!-- <input type="password" class="form-control" id="inputPassword4" placeholder="Password"> -->
+                        <select class="form-control" id="inputunit2">
+                            <option id="km1">Kilometer</option>
+                            <option id="ml1">Miles</option>
+                            <option id="mtr1">Meters</option>
+                            <option id="cm1">Centimeter</option>
+                            <option id="mm1">Milimeter</option>
+                        </select>
+                    </div>
+                </div>
+                <!-- <button type="submit" class="btn btn-primary">Sign in</button> -->
+            </form>
+        </div>
+        <!-- partial -->
+        <script src='https://code.jquery.com/jquery-3.4.1.min.js'></script>
+        <script src='https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js'></script>
+        <script src='https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.min.js'></script>
+        <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script> -->
+        <script src="./script.js"></script>
+        <script>
+            $('#inputunit1').on('change', function(e) {
+                var optionSelected = $('#inputunit1').find(":selected").text();
+
+                to_selected(optionSelected);
+
+            });
+            $('#inputunit2').on('change', function(e) {
+                var optionSelected = $('#inputunit2').find(":selected").text();
+                from_selected(optionSelected);     
+
+            });
+
+            function to_selected(selected) {
+                $.ajax({
+                    type: 'POST',
+                    url: 'ld.php',
+                    data: {
+                        'selectedItem': selected
+                    },
+                    success: function(response) {
+                        // content.html(response);
+                    }
+                });
+            }
+
+            function from_selected(selected) {
+                $.ajax({
+                    type: 'POST',
+                    url: 'ld.php',
+                    data: {
+                        'selectedItem': selected
+                    },
+                    success: function(response) {
+                        // content.html(response);
+                    }
+                });
+
+                }
+            // }
+            $('#input1').on('input', function(e) {
+                textChanged()
+                alert('Changed!')
+            });
+        </Script>
+    </body>
+
+    </html>
+
+</body>
+
+</html>
